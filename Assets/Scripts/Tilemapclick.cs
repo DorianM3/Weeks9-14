7233061e5678d8095eda17e6e495dedb56e049e9
Dorian Muhametaj <muhameta@shernet.sheridancollege.ca>
2025-04-03 14:50:42 -0400

    Made it so the knight walks from one part of the map to the next when you click there

--- Assets/Scripts/Tilemapclick.cs
+++ Assets/Scripts/Tilemapclick.cs
@@ -6,13 +6,26 @@ using UnityEngine.UIElements;
 
 public class Tilemapclick : MonoBehaviour
 {
+    SpriteRenderer sr;
+    Animator animator;
+    public float speed = 2;
+    public bool canRun = true;
+
     public Tilemap map;
 
     public Tile grass;
-    public Tile stone; 
+    public Tile stone;
+
+    Vector2 currentpos;
+    Vector2 direction;
+
+    float leftOrRight;
+    float upOrDown; 
     // Start is called before the first frame update
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         
     }
 
@@ -23,9 +36,32 @@ public class Tilemapclick : MonoBehaviour
         Vector3Int grid = map.WorldToCell(mousepos); 
         if (Input.GetMouseButtonDown(0))
         {
-            if(map.GetTile(grid) == stone)
+            direction = mousepos;
+            currentpos = transform.position;
+
+            if(direction.x - currentpos.x > 0)
             {
-                transform.position = mousepos; 
+                leftOrRight = 1; 
+            }
+
+            else
+            {
+                leftOrRight = -1;
+            }
+
+            if(direction.y - currentpos.y > 0)
+            {
+                upOrDown = 1;
+            }
+
+            else
+            {
+                upOrDown = -1;
+            }
+
+            if (map.GetTile(grid) == stone)
+            {
+                
             }
 
             else if(map.GetTile(grid) == grass)
@@ -33,5 +69,36 @@ public class Tilemapclick : MonoBehaviour
                 Debug.Log("Stay off the grass!"); 
             }
         }
+
+        sr.flipX = (direction.x < currentpos.x);
+
+        animator.SetFloat("movement", Mathf.Abs(direction.x));
+
+        if (currentpos.x < direction.x && leftOrRight == 1)
+        {
+            currentpos.x += speed * Time.deltaTime;
+            transform.position = currentpos;
+        }
+
+        else if(currentpos.x > direction.x && leftOrRight == -1)
+        {
+            currentpos.x -= speed * Time.deltaTime;
+            transform.position = currentpos;
+        }
+
+        if(currentpos.y < direction.y && upOrDown == 1)
+        {
+            currentpos.y += speed * Time.deltaTime;
+            transform.position = currentpos; 
+        }
+
+        else if (currentpos.y > direction.y && upOrDown == -1)
+        {
+            currentpos.y -= speed * Time.deltaTime;
+            transform.position = currentpos;
+        }
+
+
+
     }
 }
